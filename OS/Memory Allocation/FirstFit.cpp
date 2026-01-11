4162ab56b105564557a7c67975315730860e9146
Enosh Michael Gomes <149785264+Enosh-Gomes@users.noreply.github.com>
2026-01-11 11:10:38 +0530

    Add files via upload

---
+++ OS/Memory Allocation/FirstFit.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <climits>
+using namespace std;
+
+class SegmentRegion {
+public:
+    int max_space;
+    int free_space;
+    vector<int> segments;
+    SegmentRegion() {}
+};
+
+class Process {
+public:
+    int process_number;
+    int process_size;
+    Process() {}
+};
+
+int Process_number(SegmentRegion region[], int i, int j, Process process[], int m) {
+    int k;
+    for(k = 0; k < m; k++) {
+        if(region[i].segments[j] == process[k].process_size) {
+            break;
+        }
+    }
+    return process[k].process_number;
+}
+
+void displayMemoryStatus(SegmentRegion region[], int n, Process process[], int m) {
+    cout << "\n----------------------------------------------------------------------------------------";
+    cout << "\n| Process | Process Size (in KB) | Segment Capacity (in KB) | Memory Available (in KB) |";
+    cout << "\n----------------------------------------------------------------------------------------\n";
+
+    for (int i = 0; i < n; i++) {
+        int l = region[i].segments.size();
+        for (int j = 0; j < l; j++) {
+            cout << "|" << setw(5) << Process_number(region, i, j, process, m) << setw(5) << " |"
+                << setw(12) << region[i].segments[j] << setw(12) << " | "
+                << setw(14) << region[i].max_space << setw(12) << " |"
+                << setw(14) << region[i].free_space << setw(14) << " |\n";
+        }
+        if (l == 0) {
+            cout << "|" << setw(5) << "-" << setw(5) << " |"
+                << setw(11) << "-" << setw(13) << " | "
+                << setw(14) << region[i].max_space << setw(12) << " |"
+                << setw(14) << region[i].free_space << setw(14) << " |\n";
+        }
+    }
+    cout << "----------------------------------------------------------------------------------------\n";
+}
+
+void firstFitAllocate(SegmentRegion region[], int n, int process_size, vector<string>& logs) {
+    bool allocated = false;
+
+    for (int i = 0; i < n; i++) {
+        if (region[i].free_space >= process_size) {
+            region[i].segments.push_back(process_size);
+            region[i].free_space -= process_size;
+
+            string log = "Process of size " + to_string(process_size)
+                        + " KB allocated in segment of size "
+                        + to_string(region[i].max_space) + " KB";
+            logs.push_back(log);
+            allocated = true;
+            break;
+        }
+    }
+
+    if (!allocated) {
+        string log = "No sufficient space available for process of size "
+                    + to_string(process_size) + " KB";
+        logs.push_back(log);
+    }
+}
+
+int main() {
+    int n, m;
+
+    cout << "\nEnter number of free segments: ";
+    cin >> n;
+    SegmentRegion region[n];
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter segment capacity for segment " << i << " (in KB): ";
+        cin >> m;
+        region[i].max_space = m;
+        region[i].free_space = m;
+        region[i].segments = vector<int>(0);
+    }
+
+    cout << "\nEnter number of processes: ";
+    cin >> m;
+
+    Process process[m];
+
+    cout << "Enter process sizes (in KB):\n";
+    for (int i = 0; i < m; i++) {
+        process[i].process_number = i;
+        cout << "Process " << i << ": ";
+        cin >> process[i].process_size;
+    }
+
+    vector<string> logs;
+
+    for (Process p : process)
+        firstFitAllocate(region, n, p.process_size, logs);
+
+    displayMemoryStatus(region, n, process, m);
+
+    cout << endl;
+    for (string log : logs)
+        cout << log << endl;
+
+    return 0;
+}
+
+
+/*
+5
+100
+500
+200
+300
+600
+4
+220
+420
+120
+426
+*/
